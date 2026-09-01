4b2a860479f0b8e6309d13c270b00c62e9aaf564
sanskrutiishinde <sanskruti.1251040258@vit.edu>
2026-09-01 21:11:03 +0530

    Phase-3 : Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-print("✅ Study Assistant - Environment loaded")
 from utils.audio_processor import get_audio_path
+from utils.transcriber import transcribe, save_transcript
 
-# Test with a short YouTube video
-audio = get_audio_path("https://www.youtube.com/watch?v=xlYJhtL0qbQ")
-print(f"Audio saved at: {audio}")
+# Step 1 - Get audio
+audio = get_audio_path(r"C:\Users\frmxg\Downloads\English_Vertical_Sample (2).mp4")
+
+# Step 2 - Transcribe
+transcript = transcribe(audio)
+
+# Step 3 - Save
+path = save_transcript(transcript, audio)
+
+print(f"\n📄 Transcript preview:")
+print(transcript[:500])
