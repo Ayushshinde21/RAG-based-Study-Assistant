c26fde75e354530baf749f488f406f7e75b91b4a
sanskrutiishinde <sanskruti.1251040258@vit.edu>
2026-09-01 21:07:14 +0530

    Phase-2 : Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 print("✅ Study Assistant - Environment loaded")
+from utils.audio_processor import get_audio_path
+
+# Test with a short YouTube video
+audio = get_audio_path("https://www.youtube.com/watch?v=xlYJhtL0qbQ")
+print(f"Audio saved at: {audio}")
